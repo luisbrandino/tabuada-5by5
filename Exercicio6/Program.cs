17335689d9ee13bef7217100dfac0fb30bfe66cf
luisbrandino <luisbrandino.contato@gmail.com>
2024-04-18 15:42:50 -0300

    Add project files.

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,12 @@
+﻿// faça um programa que leia um numero inteiro positivo e imprima a sua tabuada
+
+int number = 0;
+
+while (number <= 0)
+{
+    Console.Write("Informe um número inteiro e positivo para mostrar sua tabuada: ");
+    number = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{number} x {i} = {number * i}");
